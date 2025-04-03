5d632eb06d121bc4f3e21ffe79268c6d1be68efa
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-03 14:08:06 -0400

    made camera prefab

--- Assets/Scripts/Frame.cs
+++ Assets/Scripts/Frame.cs
@@ -3,20 +3,19 @@ using UnityEngine.Rendering;
 
 public class Frame : MonoBehaviour
 {
-    public float top;
     public float bottom;
+    public float top;
     public float left;
     public float right;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Update()
     {
         Debug.Assert(left < right && bottom < top, "Invalid frame size");
-        float x = (left + right)/2;
-        float y = (bottom + top)/2;
+        float x = (left + right) / 2;
+        float y = (bottom + top) / 2;
         float height = top - bottom;
         float width = right - left;
         float aspectRatio = Screen.width / ((float)Screen.height);
-        float widthRatio = width/aspectRatio;
+        float widthRatio = width / aspectRatio;
 
         transform.position = new Vector3(x, y, -10);
 
@@ -25,12 +24,5 @@ public class Frame : MonoBehaviour
             gameObject.GetComponent<Camera>().orthographicSize = widthRatio / 2;
         else
             gameObject.GetComponent<Camera>().orthographicSize = height / 2;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
