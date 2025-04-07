f018f154f423f95a884e7be5719713bc2fa3bebd
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-07 14:39:42 -0400

    make animations activate when moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
+    public Animator animator;
     public float speed = 5f;
     void Update()
     {
@@ -10,6 +11,14 @@ public class PlayerController : MonoBehaviour
         float Vertical = Input.GetAxisRaw("Vertical");
         Vector2 movement = new Vector2(Horizontal, Vertical);
         movement = Vector2.ClampMagnitude(movement, 1);
-        rigidbody.linearVelocity = movement * Time.deltaTime * speed;
+        rigidbody.linearVelocity = movement * speed * Time.deltaTime;
+        animator.SetFloat("x", movement.x);
+        animator.SetFloat("y", movement.y);
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("lastX", movement.x);
+            animator.SetFloat("lastY", movement.y);
+        }
     }
 }
+
