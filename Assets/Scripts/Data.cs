9f1f48828f19403f5a04d827c31d781699f03933
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-08 19:55:19 -0400

    added stair angle to info nodes

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 public class Data : MonoBehaviour
 {
     public float height;
+    public float stairAngle;
     public bool isStair;
 }
--- Assets/Scripts/Info.cs
+++ Assets/Scripts/Info.cs
@@ -4,7 +4,6 @@ public class Info : Data
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(gameObject);
         if (collision.CompareTag("Player"))
         {
             Data player = collision.GetComponent<Data>();
