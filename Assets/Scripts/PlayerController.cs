e99c9c7912a725598c6e4a1df3e9c60023aa167d
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-09 15:41:16 -0400

    dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,29 @@
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
+    public Data data;
+    public LayerMask dataLayer;
     public float speed = 5f;
+    
+
     void Update()
     {
         float Horizontal = Input.GetAxisRaw("Horizontal");
         float Vertical = Input.GetAxisRaw("Vertical");
         Vector2 movement = new Vector2(Horizontal, Vertical);
         movement = Vector2.ClampMagnitude(movement, 1);
-        rigidbody.linearVelocity = movement * Time.deltaTime * speed;
+        rigidbody.linearVelocity = movement * speed;
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Vector3 Dir = movement.normalized * 3;
+            Data node = Physics2D.OverlapPoint(transform.position + Dir, dataLayer).GetComponent<Info>();
+            if (node == null)
+                return;
+            if (data.height != node.height)
+                return;
+            transform.position += Dir;
+        }
     }
 }
