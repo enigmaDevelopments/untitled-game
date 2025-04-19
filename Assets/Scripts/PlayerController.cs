ef19fd94133bca58c75fdbc2248b5ecff653f81f
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-19 11:44:27 -0400

    fixed merge errors

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,15 +30,13 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetButtonDown("Jump")) {
             Vector2 pos = transform.position + (Vector3)(movement.normalized * 3);
-            pos.y -= .5f;
-            pos = new Vector2(Mathf.Floor(pos.x) + .5f, Mathf.Floor(pos.y) + .5f);
+            pos = new Vector2(Mathf.Floor(pos.x) + .5f, Mathf.Floor(pos.y) +.5f);
             Collider2D node = Physics2D.OverlapPoint(pos, dataLayer);
             if (node == null)
                 return;
             Data info = node.GetComponent<Info>();
             if (data.height != info.height||data.isStair||info.isStair)
                 return;
-            pos.y += .5f;
             transform.position = pos;
         }
     }
