02eaf18aff18e043887a91a6e730c70e2b7deeca
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-23 10:57:14 -0400

    made hidden appear if on correct layer

--- Assets/Scripts/Hidden.cs
+++ Assets/Scripts/Hidden.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Hidden : MonoBehaviour
 {
     public GameObject draw;
+    public Data data;
     void Start()
     {
         draw.SetActive(false);
@@ -10,15 +11,12 @@ public class Hidden : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {
-            draw.SetActive(true);
-        }
+            if (data.height == collision.GetComponent<Data>().height)
+                draw.SetActive(true);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {
             draw.SetActive(false);
-        }
     }
 }
