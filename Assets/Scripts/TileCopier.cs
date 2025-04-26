fe15e1c816695985fcf7ed657c35c252e4dc5bd1
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-26 18:44:43 -0400

    made new tile map based off collitions from detector tile map

---
+++ Assets/Scripts/TileCopier.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileCopier : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Transform newLayer = Instantiate(gameObject, transform.parent).transform;
+        Destroy(newLayer.GetComponent<TileCopier>());
+        newLayer.name = "Collitions";
+        int layer = LayerMask.NameToLayer("Default");
+        newLayer.gameObject.layer = layer;
+        foreach (Transform child in newLayer)
+        {
+            child.GetComponent<CompositeCollider2D>().isTrigger = false;
+            child.GetComponent<TilemapCollider2D>().enabled = false;
+            child.gameObject.layer = layer;
+        }
+    }
+}
