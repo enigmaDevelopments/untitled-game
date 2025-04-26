1fdad242fbdb7381a555d0efb746eb988e79c4d9
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-26 17:11:55 -0400

    player can stand on box

--- Assets/Scripts/StopBox.cs
+++ Assets/Scripts/StopBox.cs
@@ -4,17 +4,20 @@ public class StopBox : MonoBehaviour
 {
     public Rigidbody2D rb;
     public SpriteRenderer spriteRenderer;
+    public Data data;
     public bool hidden = false;
     private Transform player;
     private PlayerController playerController;
+    private Data playerData;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         playerController = player.GetComponent<PlayerController>();
+        playerData = player.GetComponent<Data>();
     }
     private void Update()
     {
-        spriteRenderer.sortingLayerName = (player.position.y < transform.position.y-.05f ? "collition" : "walk behind") + (hidden ? "-hidden" : "");
+        spriteRenderer.sortingLayerName = (player.position.y < transform.position.y-.05f || data.height == playerData.height? "collition" : "walk behind") + (hidden ? "-hidden" : "");
     }
     private void FixedUpdate()
     {
