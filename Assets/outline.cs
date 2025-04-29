e0492b067da8985880e2525c201a5ce945aac336
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-29 14:57:19 -0400

    made outlines for each level

--- Assets/outline.cs
+++ Assets/outline.cs
@@ -1,18 +1,29 @@
 using System;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class outline : MonoBehaviour
 {
     void Start()
     {
+        GameObject lineObj = transform.GetChild(0).gameObject;
         GridInfo grides = transform.parent.parent.GetComponent<GridInfo>();
-        LineRenderer line = GetComponent<LineRenderer>();
-        CompositeCollider2D myCollider = grides.detectors.transform.GetChild(0).GetComponent<CompositeCollider2D>();
-        Vector2[] points = new Vector2[myCollider.GetPathPointCount(0)];
-        myCollider.GetPath(0, points);
-        Vector3[] points3D = Array.ConvertAll(points, i => new Vector3(i.x, i.y, 0));
-        line.positionCount = points3D.Length;
-        line.SetPositions(points3D);    
+         
+        foreach (Transform child in grides.detectors.transform)
+        {
+            if (child.GetComponent<Data>().isStair)
+                continue;
+            CompositeCollider2D collider = child.GetComponent<CompositeCollider2D>();
+            for (int i = 0; i < collider.pathCount; i++)
+            {
+                LineRenderer line = Instantiate(lineObj, transform).GetComponent<LineRenderer>();
+                Vector2[] points = new Vector2[collider.GetPathPointCount(i)];
+                collider.GetPath(i, points);
+                Vector3[] points3D = Array.ConvertAll(points, i => new Vector3(i.x, i.y, 0));
+                line.positionCount = points3D.Length;
+                line.SetPositions(points3D);
+                line.Simplify(.5f);
+            }
+        }
+        Destroy(lineObj);
     }
 }
