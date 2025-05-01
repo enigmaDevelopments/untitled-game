cb38db23f08dd99828806e50d3defec131b24bde
enigmaDevelopments <enigmarezac@gmail.com>
2025-04-30 20:40:23 -0400

    made timer restart game

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,22 +1,42 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
-    public static float timer = 120f;
+    public float time = 120f;
+    public float timer = 120f;
+    private static bool isTimerActive = false;
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        if (isTimerActive)
+            Destroy(gameObject);
+        else
+        {
+            isTimerActive = true;
+            timer = time;
+            DontDestroyOnLoad(gameObject);
+        }
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         timer -= Time.deltaTime;
-        int minutes = (int)(timer / 60);
-        int seconds = (int)(timer % 60);
-        timerText.text = $"{minutes}:{seconds}";
-
+        if (time <= 60)
+            timerText.text = $"{(int)timer}";
+        else
+            timerText.text = string.Format("{0:0}:{1:00}", (int)(timer / 60), (int)(timer % 60));
+        if (timer <= 0)
+        {
+            timerText.text = "0:00";
+            SceneManager.LoadScene(0);
+            timer = time;
+            GameVaribles.playerPos = Vector2.zero;
+            GameVaribles.height = 0;
+            GameVaribles.stairAngle = 0;
+            GameVaribles.isStair = false;
+        }
     }
 }
